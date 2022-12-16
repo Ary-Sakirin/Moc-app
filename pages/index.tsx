39f7be138c0e669d94764b1cdc3a7d3cf9fdca34
ArySakirin <arysakirin29@gmai.com>
2022-12-16 11:00:30 +0700

    edit file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
-import GetStart from "../src/screens/home/getStart";
-import Testimonial from "../src/screens/home/testimonial";
-import Title from "../src/screens/home/title";
-import Blog from "../src/screens/home/blog";
+import GetStart from "../src/screens/home/GetStart";
+import Testimonial from "../src/screens/home/Testimonial";
+import Title from "../src/screens/home/Title";
+import Blog from "../src/screens/home/Blog";
 
 export default function Home() {
   return (
